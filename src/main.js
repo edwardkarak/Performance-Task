9669228d40e56fc65fbbd51f738d5c25226ea376
Edward Karak <52176416+edwardkarak@users.noreply.github.com>
2021-03-11 18:34:59 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,540 @@
+students = [
+  {
+    "_id": {
+      "$oid": "5dc194991c9d4400004c640a"
+    },
+    "name": "Stuart Dent",
+    "grade": 6,
+    "scores": [
+      {
+        "subject": "Math",
+        "value": 85
+      },
+      {
+        "subject": "English",
+        "value": 65
+      },
+      {
+        "subject": "Science",
+        "value": 70
+      },
+      {
+        "subject": "Social Studies",
+        "value": 95
+      }
+    ]
+  },
+  {
+    "_id": {
+      "$oid": "5dc1956a1c9d4400004c640c"
+    },
+    "name": "Christine Walls",
+    "grade": 7,
+    "scores": [
+      {
+        "subject": "Math",
+        "value": 95
+      },
+      {
+        "subject": "English",
+        "value": 80
+      },
+      {
+        "subject": "Science",
+        "value": 85
+      },
+      {
+        "subject": "Social Studies",
+        "value": 95
+      }
+    ]
+  },
+  {
+    "_id": {
+      "$oid": "5dc196c71c9d4400004c640d"
+    },
+    "name": "Mirza Carney",
+    "grade": 8,
+    "scores": [
+      {
+        "subject": "Math",
+        "value": 55
+      },
+      {
+        "subject": "English",
+        "value": 85
+      },
+      {
+        "subject": "Science",
+        "value": 75
+      },
+      {
+        "subject": "Social Studies",
+        "value": 65
+      }
+    ]
+  },
+  {
+    "_id": {
+      "$oid": "5dc1971e1c9d4400004c640e"
+    },
+    "name": "Ishmael Wade",
+    "grade": 6,
+    "scores": [
+      {
+        "subject": "Math",
+        "value": 50
+      },
+      {
+        "subject": "English",
+        "value": 90
+      },
+      {
+        "subject": "Science",
+        "value": 95
+      },
+      {
+        "subject": "Social Studies",
+        "value": 60
+      }
+    ]
+  },
+  {
+    "_id": {
+      "$oid": "5dc197461c9d4400004c640f"
+    },
+    "name": "Aanya Legge",
+    "grade": 8,
+    "scores": [
+      {
+        "subject": "Math",
+        "value": 75
+      },
+      {
+        "subject": "English",
+        "value": 75
+      },
+      {
+        "subject": "Science",
+        "value": 75
+      },
+      {
+        "subject": "Social Studies",
+        "value": 75
+      }
+    ]
+  },
+  {
+    "_id": {
+      "$oid": "5dc197651c9d4400004c6410"
+    },
+    "name": "Elsie-Rose Ramirez",
+    "grade": 7,
+    "scores": [
+      {
+        "subject": "Math",
+        "value": 65
+      },
+      {
+        "subject": "English",
+        "value": 95
+      },
+      {
+        "subject": "Science",
+        "value": 100
+      },
+      {
+        "subject": "Social Studies",
+        "value": 65
+      }
+    ]
+  },
+  {
+    "_id": {
+      "$oid": "5dc1979b1c9d4400004c6411"
+    },
+    "name": "Reanne Simons",
+    "grade": 6,
+    "scores": [
+      {
+        "subject": "Math",
+        "value": 95
+      },
+      {
+        "subject": "English",
+        "value": 75
+      },
+      {
+        "subject": "Science",
+        "value": 60
+      },
+      {
+        "subject": "Social Studies",
+        "value": 80
+      }
+    ]
+  },
+  {
+    "_id": {
+      "$oid": "5dc198051c9d4400004c6412"
+    },
+    "name": "Amelia-Rose Mullins",
+    "grade": 7,
+    "scores": [
+      {
+        "subject": "Math",
+        "value": 60
+      },
+      {
+        "subject": "English",
+        "value": 90
+      },
+      {
+        "subject": "Science",
+        "value": 90
+      },
+      {
+        "subject": "Social Studies",
+        "value": 85
+      }
+    ]
+  },
+  {
+    "_id": {
+      "$oid": "5dc198291c9d4400004c6413"
+    },
+    "name": "Shae Paterson",
+    "grade": 8,
+    "scores": [
+      {
+        "subject": "Math",
+        "value": 65
+      },
+      {
+        "subject": "English",
+        "value": 70
+      },
+      {
+        "subject": "Science",
+        "value": 80
+      },
+      {
+        "subject": "Social Studies",
+        "value": 65
+      }
+    ]
+  },
+  {
+    "_id": {
+      "$oid": "5dc1984a1c9d4400004c6414"
+    },
+    "name": "Kailan Quintero",
+    "grade": 6,
+    "scores": [
+      {
+        "subject": "Math",
+        "value": 65
+      },
+      {
+        "subject": "English",
+        "value": 80
+      },
+      {
+        "subject": "Science",
+        "value": 75
+      },
+      {
+        "subject": "Social Studies",
+        "value": 60
+      }
+    ]
+  },
+  {
+    "_id": {
+      "$oid": "5dc198871c9d4400004c6415"
+    },
+    "name": "Rahim Williams",
+    "grade": 7,
+    "scores": [
+      {
+        "subject": "Math",
+        "value": 95
+      },
+      {
+        "subject": "English",
+        "value": 90
+      },
+      {
+        "subject": "Science",
+        "value": 75
+      },
+      {
+        "subject": "Social Studies",
+        "value": 70
+      }
+    ]
+  },
+  {
+    "_id": {
+      "$oid": "5dc199101c9d4400004c6416"
+    },
+    "name": "Davey Barrow",
+    "grade": 8,
+    "scores": [
+      {
+        "subject": "Math",
+        "value": 50
+      },
+      {
+        "subject": "English",
+        "value": 70
+      },
+      {
+        "subject": "Science",
+        "value": 80
+      },
+      {
+        "subject": "Social Studies",
+        "value": 65
+      }
+    ]
+  },
+  {
+    "_id": {
+      "$oid": "5dc199561c9d4400004c6417"
+    },
+    "name": "Rosa Cervantes",
+    "grade": 6,
+    "scores": [
+      {
+        "subject": "Math",
+        "value": 70
+      },
+      {
+        "subject": "English",
+        "value": 80
+      },
+      {
+        "subject": "Science",
+        "value": 90
+      },
+      {
+        "subject": "Social Studies",
+        "value": 100
+      }
+    ]
+  },
+  {
+    "_id": {
+      "$oid": "5dc199731c9d4400004c6418"
+    },
+    "name": "Aneeka Leal",
+    "grade": 7,
+    "scores": [
+      {
+        "subject": "Math",
+        "value": 65
+      },
+      {
+        "subject": "English",
+        "value": 70
+      },
+      {
+        "subject": "Science",
+        "value": 75
+      },
+      {
+        "subject": "Social Studies",
+        "value": 80
+      }
+    ]
+  },
+  {
+    "_id": {
+      "$oid": "5dc199991c9d4400004c6419"
+    },
+    "name": "Helin Holder",
+    "grade": 8,
+    "scores": [
+      {
+        "subject": "Math",
+        "value": 80
+      },
+      {
+        "subject": "English",
+        "value": 75
+      },
+      {
+        "subject": "Science",
+        "value": 70
+      },
+      {
+        "subject": "Social Studies",
+        "value": 65
+      }
+    ]
+  },
+  {
+    "_id": {
+      "$oid": "5dc199ba1c9d4400004c641a"
+    },
+    "name": "Zayn Bains",
+    "grade": 6,
+    "scores": [
+      {
+        "subject": "Math",
+        "value": 65
+      },
+      {
+        "subject": "English",
+        "value": 95
+      },
+      {
+        "subject": "Science",
+        "value": 70
+      },
+      {
+        "subject": "Social Studies",
+        "value": 85
+      }
+    ]
+  },
+  {
+    "_id": {
+      "$oid": "5dc199d71c9d4400004c641b"
+    },
+    "name": "Monty Bevan",
+    "grade": 7,
+    "scores": [
+      {
+        "subject": "Math",
+        "value": 75
+      },
+      {
+        "subject": "English",
+        "value": 55
+      },
+      {
+        "subject": "Science",
+        "value": 80
+      },
+      {
+        "subject": "Social Studies",
+        "value": 80
+      }
+    ]
+  },
+  {
+    "_id": {
+      "$oid": "5dc199f61c9d4400004c641c"
+    },
+    "name": "Mia Hanson",
+    "grade": 8,
+    "scores": [
+      {
+        "subject": "Math",
+        "value": 75
+      },
+      {
+        "subject": "English",
+        "value": 75
+      },
+      {
+        "subject": "Science",
+        "value": 80
+      },
+      {
+        "subject": "Social Studies",
+        "value": 60
+      }
+    ]
+  },
+  {
+    "_id": {
+      "$oid": "5dc19a281c9d4400004c641d"
+    },
+    "name": "Ewan Rosario",
+    "grade": 6,
+    "scores": [
+      {
+        "subject": "Math",
+        "value": 60
+      },
+      {
+        "subject": "English",
+        "value": 90
+      },
+      {
+        "subject": "Science",
+        "value": 55
+      },
+      {
+        "subject": "Social Studies",
+        "value": 90
+      }
+    ]
+  },
+  {
+    "_id": {
+      "$oid": "5dc19a531c9d4400004c641e"
+    },
+    "name": "Gino Dickerson",
+    "grade": 7,
+    "scores": [
+      {
+        "subject": "Math",
+        "value": 75
+      },
+      {
+        "subject": "English",
+        "value": 70
+      },
+      {
+        "subject": "Science",
+        "value": 80
+      },
+      {
+        "subject": "Social Studies",
+        "value": 65
+      }
+    ]
+  }
+];
 
+function groupByGrade(students)
+{
+	return students.reduce((acc, obj) => {
+      const property = obj["grade"];
+      acc[property] = acc[property] || [];
+      acc[property].push(obj);
+      return acc;
+    }, {});
+}
+
+function findLowestAverages(students)
+{
+	var min_grades = {};
+	
+	// min_grade[g] stores the lowest recorded grade for a student in the grade g
+	min_grades[6] = 100, min_grades[7] = 100, min_grades[8] = 100;
+
+	for (var i = 0; i < students.length; i++) {
+		var student = students[i];
+
+		// Calculate student average
+		avg = 0;
+			
+		for (var s = 0; s < student["scores"].length; s++)
+			avg += student["scores"][s]["value"];
+
+		avg /= student["scores"].length;
+
+		// Update min_grades if avg is less than the one in min_grades
+		if (avg < min_grades[student["grade"]])
+			min_grades[student["grade"]] = avg;
+	}
+
+	return min_grades;
+}
+
+document.getElementById("output").innerHTML = "<strong>Task 1</strong>: <br>" + JSON.stringify(groupByGrade(students)) + "<hr><strong>Task 2</strong>:<br>" + JSON.stringify(findLowestAverages(students));
